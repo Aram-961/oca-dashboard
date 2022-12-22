2fa350e3e02d6716cbd9cef44d26adba9943a2c9
aramistamboulian961 <aramistamboulian961@gmail.com>
2022-12-22 22:50:26 +0200

    adding dropdown menu

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -3,7 +3,9 @@ import BuyOca from "../../src/components/BuyOCA";
 const OcaDashboard = () => {
     return (
         <section className="hero is-fullheight p-5" style={{background: '#191B1F'}}>
+        <div className="container">
                 <BuyOca />
+                </div>
         </section>
     )
 }
--- src/components/BuyOCA/index.js
+++ src/components/BuyOCA/index.js
@@ -1,12 +1,13 @@
 import { useState } from 'react';
+import { Dropdown } from 'primereact/dropdown';
 import styles from './dashboard.module.css'
 
 const BuyOca = () => {
     const [increase, setIncrease] = useState('OCA Amount');
     const [decrease, setDecrease] = useState('0')
-    const { buttonsPanel } = styles;
+    const { buttonsPanel, amountOca, connectWallet } = styles;
     return (
-        <div className='column is-3'>
+        <div className='column'>
             <div className='box' style={{ background: '#21242C', padding: '20px' }}>
                 <h1 className='is-size-3 has-text-white has-text-weight-semibold'>Buy</h1>
                 <div className='box is-flex is-align-items-center is-justify-content-space-between my-4' style={{ background: '#2D2F3A' }}>
@@ -33,22 +34,49 @@ const BuyOca = () => {
                 </div>
 
                 <div className='buyPanel'>
-                    <div className='box'>
-                        <div className='is-flex is-align-items-center'>
-                            <div className=''>
+                    <div className='box' style={{ background: '#2D2F3A' }}>
+                        <div className='is-flex is-justify-content-center is-align-items-center'>
+                            {/* <div className=''>
                                 <button className={`${buttonsPanel}`}>
                                     +
                                 </button>
-                            </div>
-                            <div>
+                            </div> */}
+                            <div className={`${amountOca}`}>
                                 Amount OCA
                             </div>
-                            <div>
+                            {/* <div>
                                 <button className={`${buttonsPanel}`}>
                                     -
                                 </button>
-                            </div>
+                            </div> */}
                         </div>
+                    </div>
+                </div>
+                <div className='currencyChange py-3'>
+                    <div className='box is-flex is-align-items-center is-justify-content-space-between' style={{ background: '#2D2F3A' }}>
+                        <div className=''>
+                            <h1 className='has-text-white'>0</h1>
+                            <h2>
+                                Total
+                                <span>25% OFF</span>
+                            </h2>
+                        </div>
+
+                        {/* <select name='currency' id='cryptoCurrency'>
+                            <option value="eth">
+                                Eth
+                            </option>
+                            <option value="USDC">USDC</option>
+                            <option value="USDT">USDT</option>
+                            <option value="BUSD">BUSD</option>
+                        </select> */}
+                    </div>
+                </div>
+                <div className={`${connectWallet} py-2 has-text-centered`}>
+                    <div className='box' style={{ background: '#3B62CC' }}>
+                        <a className={`${connectWallet} has-text-white`}>
+                            Connect Wallet
+                        </a>
                     </div>
                 </div>
             </div>
