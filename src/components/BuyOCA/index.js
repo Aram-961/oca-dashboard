fb1fb8f3b0b8f9a37d3afe162e57df86336e057d
aramistamboulian961 <aramistamboulian961@gmail.com>
2022-12-26 19:07:38 +0200

    pushing input oca amount

--- src/components/BuyOCA/index.js
+++ src/components/BuyOCA/index.js
@@ -1,26 +1,29 @@
 import { useState } from "react";
 import { Dropdown } from "primereact/dropdown";
-import styles from "./dashboard.module.css";
+import styles from "./buyoca.module.css";
 
 const BuyOca = () => {
+    const [amount, setAmount] = useState(0);
     const [increase, setIncrease] = useState("OCA Amount");
     const [decrease, setDecrease] = useState(0);
-    const { buttonsPanel, amountOca, connectWallet } = styles;
+    const { buttonsPanel, panelInput, panelScreen, BuyPanel, amountOca, connectWallet } = styles;
 
     const MaxAmount = 10000;
 
     // use ternary op
     const handleIncrease = () => {
         if (+ increase < MaxAmount) {
-            setIncrease(+increase + 1);
+            setIncrease(+ increase + 1);
         }
+        console.log(increase);
     };
 
     // use ternary op
     const handleDecrease = () => {
         if (+ decrease > 1) {
-            setDecrease(+decrease - 1);
+            setDecrease(+ decrease - 1);
         }
+        console.log(decrease);
     };
     return (
         <div className="column">
@@ -49,26 +52,31 @@ const BuyOca = () => {
                         <img src="/media/fees.svg" alt="" />
                     </div>
                 </div>
-
-                <div className="is-flex is-align-content-center">
-                    <img src="/media/Other 07.svg" alt="" />
-                </div>
-
-                <div className="buyPanel">
-                    <div className="box" style={{ background: "#2D2F3A" }}>
-                        <div className="is-flex is-justify-content-center is-align-items-center">
-                            {/* <div className=''>
-                                <button className={`${buttonsPanel}`}>
-                                    +
-                                </button>
-                            </div> */}
-                            <div className={`${amountOca}`}>
-                                <input
-                                    // id={input_amount}
-                                    type="text"
-                                    placeholder="Name"
-                                />
-                            </div>
+                {/* img here  */}
+                <div className={panelScreen} />
+                <div className={BuyPanel}>
+                    <div
+                        className="field is-flex is-justify-content-center is-align-items-center mb-0"
+                        style={{ columnGap: '10px' }}
+                    >
+                        <div className="control">
+                            <button type="button" className={buttonsPanel} onClick={handleDecrease}>
+                                -
+                            </button>
+                        </div>
+                        <div className="control flex-grow-1">
+                            <input
+                                className={panelInput}
+                                type="text"
+                                placeholder="Name"
+                            // value={formatNumber(amount)}
+                            // onChange={handleAmountChange}
+                            />
+                        </div>
+                        <div className="control">
+                            <button type="button" className={buttonsPanel} onClick={handleIncrease}>
+                                +
+                            </button>
                         </div>
                     </div>
                 </div>
@@ -85,14 +93,15 @@ const BuyOca = () => {
                             </h2>
                         </div>
 
-                        {/* <select name='currency' id='cryptoCurrency'>
-                            <option value="eth">
-                                Eth
-                            </option>
-                            <option value="USDC">USDC</option>
-                            <option value="USDT">USDT</option>
-                            <option value="BUSD">BUSD</option>
-                        </select> */}
+                        {/* <Dropdown
+                            options={CryptoItems}
+                            // itemTemplate={optionsTemplate}
+                            // valueTemplate={selectedCryptoTemplate}
+                            // onChange={onCryptoChange}
+                            // value={selectedCrypto}
+                            placeholder="Crypto"
+                            panelClassName="tab3-dropdown"
+                        /> */}
                     </div>
                 </div>
                 <div className={`${connectWallet} py-2 has-text-centered`}>
---
+++ src/static/buyoca/index.js
@@ -0,0 +1,8 @@
+const CryptoItems = [
+    { label: 'ETH', icon: '/media/logos/ETH.png', name: 'Ethereum' },
+    { label: 'USDC', icon: '/media/logos/usdc.png', name: 'Usdc' },
+    { label: 'USDT', icon: '/media/logos/usdt.png', name: 'Usdt' },
+    { label: 'BUSD', icon: '/media/logos/busd.png', name: 'Busd' },
+];
+
+export default CryptoItems;
